065dc19d8b5a46a2f397a2e510d9d3e803ad098a
Anitasilvi <anitaferdina06@gmail.com>
2023-12-10 10:36:32 +0700

    database setup dan dummy data

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 require("dotenv").config();
 const cors = require("cors");
 const express = require("express");
+const connectDB = require("./connectDB");
+const Book = require('./models/Books');
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+connectDB();
 app.use(cors());
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
